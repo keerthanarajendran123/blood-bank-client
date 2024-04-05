4fe60f2cbb9224a7b4d16d9ff8d360d9939ce8e1
keerthanarajendran123 <keerthurb9@gmail.com>
2024-04-05 19:53:55 +0530

    login credentials

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
       ) : (
         <div className="row g-0">
           <div className="col-md-8 form-banner">
-            <img src="./assets/images/banner1.jpg" alt="loginImage" />
+            <img src="./assets/images/banner1.jpg" alt="loginImage"/>
           </div>
           <div className="col-md-4 form-container" style={{backgroundColor:"#000",color:"#fff"}}>
             <Form
@@ -21,6 +21,13 @@ const Login = () => {
               submitBtn={"Login"}
               formType={"login"}
             />
+            <div className="default-credentials">
+              <p>Default credentials for testing:</p>
+              <p>Donar: donar@donar.com | Password: 123456</p>
+              <p>Organisation: org@org.com | Password: 123456</p>
+              <p>Admin: admin@admin.com | Password: 123456</p>
+              <p>Hospital: hospital@hospital.com | Password: 123456</p>
+            </div>
           </div>
         </div>
       )}
