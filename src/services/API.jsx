64a303d0d69e90aacd8ff35aad67b475a3cb6d24
keerthanarajendran123 <keerthurb9@gmail.com>
2024-04-07 08:54:42 +0530

    interceptors

--- src/services/API.jsx
+++ src/services/API.jsx
@@ -1,6 +1,23 @@
+// import axios from "axios";
+
+// const API = axios.create({ baseURL: import.meta.env.VITE_API_URL });
+
+// API.interceptors.request.use((req) => {
+//   const token = localStorage.getItem("token");
+//   if (token) {
+//     req.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return req;
+// });
+
+// export default API;
+
+// services/API.js
+
 import axios from "axios";
 
 const API = axios.create({ baseURL: import.meta.env.VITE_API_URL });
+
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
@@ -11,5 +28,4 @@ API.interceptors.request.use((req) => {
 });
 
 export default API;
-
 
